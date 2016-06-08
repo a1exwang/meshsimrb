b50e02a32e40960d232e7c5aa780a2344553d838
Alex Wang <ice_b0und@hotmail.com>
2016-06-08 21:53:28 +0800

    finish vertex without eliminate duplicate faces

--- vertex.rb
+++ vertex.rb
@@ -71,6 +71,52 @@ class Vertices
     raise "no such vertex #{id}" unless @vertices[id]
     @vertices[id][:associated_vertices]
   end
+
+  def modify_face(fid, src, dst)
+    raise "vertex #{src} is not on face #{fid}" unless @vertices[src][:faces].include?(fid)
+    @vertices[src][:faces].delete fid
+    raise "vertex #{dst} is on face #{fid}" if @vertices[dst][:faces].include?(fid)
+    @vertices[dst][:faces] << fid
+  end
+
+  def modify_line(v1, src, dst)
+    raise "vertex #{src} is not associated with #{v1}" unless @vertices[src][:associated_vertices].include?(v1)
+    @vertices[v1][:associated_vertices].delete src
+    raise ArgumentError, "vertex #{dst} is associated with #{v1}" if @vertices[dst][:associated_vertices].include?(v1)
+    @vertices[v1][:associated_vertices] << dst
+
+    @vertices[dst][:associated_vertices] << v1
+
+    raise "vertex #{v1} is not associated with #{src}" unless @vertices[src][:associated_vertices].include?(v1)
+    @vertices[src][:associated_vertices].delete v1
+  end
+
+  def to_obj
+    str = ''
+    v_index = 0
+    v_mappings = {}
+    total_size = @vertices.size
+    i = 0
+    @vertices.each do |id, v|
+      v_mappings[id] = v_index
+      v_index += 1
+      str += "v #{v[:vector].to_a.map { |x| x.to_s }.join(' ')}\n"
+      puts "formatting vertices #{(100.0 * i / total_size).round(2)}%..." if i == (0.01*total_size).round && VERBOSE >= V_NORMAL
+      i += 1
+    end
+    puts 'formatting vertices done' if VERBOSE >= V_NORMAL
+    str += "\n"
+    [str, v_mappings]
+  end
+
+  def dump_to_s
+    str = "vertices\n"
+    @vertices.each do |id, v|
+      str += "vertex #{id}, faces: #{v[:faces].sort.join(' ')}\n\tassociated: #{v[:associated_vertices].sort.join(' ')}\n"
+    end
+    str
+  end
+
 end
 
 class Lines
@@ -85,8 +131,16 @@ class Lines
   def add_lines_by_face(v1, v2, v3)
     begin
       add_line(v1, v2)
+    rescue ArgumentError => e
+      puts e if VERBOSE > V_NORMAL
+    end
+    begin
       add_line(v1, v3)
-      add_line(v2, v3)
+    rescue ArgumentError => e
+      puts e if VERBOSE > V_NORMAL
+    end
+    begin
+      add_line(v3, v2)
     rescue ArgumentError => e
       puts e if VERBOSE > V_NORMAL
     end
@@ -111,15 +165,34 @@ class Lines
 
   def delete_line(v1, v2)
     if get(v1, v2)
-      @lines[v1].delete v2
+      v1, v2 = v2, v1 if v1 > v2
+      ret = @lines[v1].delete v2
       @lines.delete v1 if @lines[v1].size == 0
+      ret
     else
       raise "trying to delete non-existing line #{v1}-#{v2}"
     end
   end
 
+  def modify_line(v1, src, dst)
+    delete_line(v1, src)
+    begin
+      add_line(v1, dst)
+    rescue ArgumentError => e
+      puts e if VERBOSE > V_NORMAL
+    end
+  end
+
   def first
     @lines.first.last.first.last[:vertices]
+  end
+
+  def dump_to_s
+    str = "lines\n"
+    each_line do |v1, v2, l|
+      str += "\t#{v1}-#{v2}\n"
+    end
+    str
   end
 
   private
@@ -152,10 +225,40 @@ class Faces
     raise "trying to get non-existing face #{id}" unless @faces[id]
     @faces[id][:vertices]
   end
+
+  def modify_face(id, src, dst)
+    raise "trying to modify non-existing face #{id}" unless @faces[id]
+    raise "trying to remove non-existing vertex #{src} on face #{id}" unless @faces[id][:vertices].include?(src)
+    @faces[id][:vertices].delete src
+    raise "trying to add existing vertex #{src} on face #{id}" if @faces[id][:vertices].include?(dst)
+    @faces[id][:vertices] << dst
+  end
+
+  def to_obj(v_mappings)
+    str = ''
+    total_size = @faces.size
+    i = 0
+    @faces.each do |_id, face|
+      v1, v2, v3 = face[:vertices].to_a
+      puts "f #{v1} #{v2} #{v3}" if VERBOSE > V_NORMAL
+      str += "f #{face[:vertices].to_a.map { |x| v_mappings[x] ? v_mappings[x] + 1 : (raise "no mapping #{x}") }.join(' ')}\n"
+      puts "formatting faces #{(100.0 * i / total_size).round(2)}%" if i == (total_size * 0.01).round && VERBOSE >= V_NORMAL
+      i += 1
+    end
+    puts 'formatting faces done' if VERBOSE >= V_NORMAL
+    str
+  end
+  def dump_to_s
+    str = "faces\n"
+    @faces.each do |id, f|
+      str += "\tf #{id}, vertices: #{f[:vertices].sort.join(' ')}\n"
+    end
+    str
+  end
 end
 
 class ObjectManager
-  def initialize(filename)
+  def initialize(file_path)
     @vertices = Vertices.new
     @lines = Lines.new
     @faces = Faces.new
@@ -164,7 +267,7 @@ class ObjectManager
     # 面: { vertices: [v_id, ...], kp: Matrix[] }
     # 线: { vertices: [v_id, ...], delta: 1.0 }
     v_id = 0
-    File.read(filename).split("\n").each do |line|
+    File.read(file_path).split("\n").each do |line|
       type, *rest = line.split(' ')
       case type
         when 'v'
@@ -178,6 +281,74 @@ class ObjectManager
       end
     end
   end
+
+  def merge_vertex(src, dst)
+    puts "\nmerging #{src} -> #{dst}" if VERBOSE >= V_NORMAL
+    # modify and delete faces
+    faces_that_has_changed = []
+    f1 = @vertices.get_associated_face_ids(src)
+    f2 = @vertices.get_associated_face_ids(dst)
+    faces_to_delete = f1 & f2
+    faces_to_modify = f1 - f2
+    faces_that_has_changed = faces_to_modify
+    # after here, f1, f2 are invalid
+    faces_to_delete.each do |fid|
+      vs = @faces.get_vertices(fid)
+      @faces.delete_face(fid)
+      @vertices.delete_face(vs, fid)
+    end
+    faces_to_modify.each do |fid|
+      @faces.modify_face(fid, src, dst)
+      @vertices.modify_face(fid, src, dst)
+    end
+
+    # modify and delete lines
+    lines_that_has_changed = []
+    lines_to_touch = @vertices.get_associated_vertex_ids(src)
+    raise "#{dst} is not associated with #{src}" unless lines_to_touch.include?(dst)
+    lines_to_modify = lines_to_touch - [dst]
+    lines_to_modify.each do |v2|
+      # the line is src-v2
+      @lines.modify_line(v2, src, dst)
+      begin
+        @vertices.modify_line(v2, src, dst)
+      rescue ArgumentError => e
+        puts e if VERBOSE > V_NORMAL
+      end
+      lines_that_has_changed << [v2, dst]
+    end
+
+    @lines.delete_line(src, dst)
+    @vertices.delete_line(src, dst)
+
+    @vertices.delete(src)
+  end
+
+  def get_a_line
+    @lines.first
+  end
+
+  def write_to_file(file_path)
+    vertices_str, v_mappings = @vertices.to_obj
+    faces_str = @faces.to_obj(v_mappings)
+    File.write(file_path, vertices_str + faces_str)
+  end
+
+  def dump_print
+    puts @vertices.dump_to_s
+    puts @lines.dump_to_s
+    puts @faces.dump_to_s
+  end
 end
 
-obj = ObjectManager.new('test_data/dinosaur.2k.obj')
+obj = ObjectManager.new('test_data/cube.obj')
+
+obj.dump_print
+
+5.times do
+  v1, v2 = obj.get_a_line
+  obj.merge_vertex(v1, v2)
+  obj.dump_print
+end
+
+obj.write_to_file('a.obj')
